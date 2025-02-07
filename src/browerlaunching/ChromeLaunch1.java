5cdd78239d4773468e52debc743824c29b13e2ae
seleniumriyaz <selenium.riyaz.shared@gmail.com>
2025-02-07 19:50:36 +0530

    Selenium Season 1 notes and programs till Feb 7th

---
+++ src/browerlaunching/ChromeLaunch1.java
@@ -0,0 +1,23 @@
+package browerlaunching;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+//import org.openqa.selenium.chrome.ChromeDriver;
+
+//import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ChromeLaunch1 {
+
+	public static void main(String[] args) {
+		
+		
+		//WebDriverManager.chromedriver().setup();
+		
+		//System.setProperty("webdriver.chrome.driver", "D:\\FLM5thDec\\jars\\chromedriver-win64 (1)\\chromedriver-win64\\chromedriver.exe");
+		
+		new ChromeDriver();
+		
+
+	}
+
+}
