ca49516fe7ec3f4b1f3d44627b24199692e97974
seleniumriyaz <selenium.riyaz.shared@gmail.com>
2025-03-05 19:34:39 +0530

    Selenium Season 2 notes and programs till Mar 5th

--- src/testng/AdactinHotelApp.java
+++ src/testng/AdactinHotelApp.java
@@ -1,49 +1,61 @@
 package testng;
 
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class AdactinHotelApp {
 	
-	@Test(priority=2)
-	public void loginTest()
+	@BeforeMethod
+	public void setUp()
 	{
 		System.out.println("Browser Started ..");
 		System.out.println("App Launched ..");
-		Assert.assertTrue(false);
-		System.out.println("Login successfull..");
+	}
+	
+	@AfterMethod
+	public void teardown()
+	{
 		System.out.println("Browser closed ..");
+	}
+	
+	@Test(priority=2)
+	public void loginTest()
+	{
+		
+		Assert.assertTrue(true);
+		System.out.println("Login successfull..");
+		
 		
 	}
 	
 	@Test(priority=0)
 	public void registrationTest()
 	{
-		System.out.println("Browser Started ..");
-		System.out.println("App Launched ..");
+		
 		System.out.println("Registration successfull..");
-		System.out.println("Browser closed ..");
+		
 		
 	}
 	
 	@Test(priority=3)
 	public void forgotPasswordTest()
 	{
-		System.out.println("Browser Started ..");
-		System.out.println("App Launched ..");
-		System.out.println("Forgot Password successfull..");
-		System.out.println("Browser closed ..");
 		
+		System.out.println("Forgot Password successfull..");
+			
 	}
 	
 	@Test(priority=4,dependsOnMethods="loginTest")
 	public void changePasswordTest()
 	{
-		System.out.println("Browser Started ..");
-		System.out.println("App Launched ..");
+		
 		System.out.println("Change Password successfull..");
-		System.out.println("Browser closed ..");
+		
 		
 	}
+	
+	
 
 }
